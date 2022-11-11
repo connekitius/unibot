f4e1bc995c2d25f37c83c6cc4d7b23c1a7840664
Arktic <77226064+Arktic-py@users.noreply.github.com>
2022-11-11 13:16:16 +0200

    made it look cooler

--- bot_events/ready.py
+++ bot_events/ready.py
@@ -1,2 +1,10 @@
+from colorama import Fore
+
+Blue = Fore.BLUE
+Green = Fore.GREEN
+
 async def on_ready():
-	print(f'Logged in!')
+	print(Green+ "Logged in!")
+	print(Blue+ "Username: ", Red+ f"{client.user.name}")
+    	print(Blue+ "id:", Red+ f"{client.user.id}")
+    	print("")
