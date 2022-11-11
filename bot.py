f34b225dc0f83afc639ae9769904efb13eb276bd
Connek <106320267+connekitius@users.noreply.github.com>
2022-11-11 11:21:05 +0000

    💥 Revamp bot.py
    🚑 Adds group/sub-commands support to command handler
    💡 Removes unnecessary comments and comments containing profanity
    🏗 Replaces all functions with single line lambdas for less code and space and more efficiency
    🎨 Improves indenting
    🔧 Adds settings.py support
    🧨 Removes unnecessary functions, modules and typing imports
    ➖ Removes colorama module usage
    🚚 Renames bot_events -> events

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
 import sys
 sys.dont_write_bytecode = True
 
-from typing import Callable, List
-from colorama import init, Fore
-from ast import ClassDef
+from typing import Callable, List, Union, Tuple, Any
+from types import FunctionType
 from datetime import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
-import discord, glob, os, importlib, inspect
-
-import settings
-
-init()
+import discord, glob, os, importlib, inspect, settings
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -19,53 +14,44 @@ intents.message_content = True
 intents.members = True
 intents.presences = True
 
-def isCallable(v):
-	return isinstance(v, Callable)
+isCallable: Callable[[Any], bool] = lambda v: isinstance(v, Callable)
+isCommand: Callable[[Any], bool] = lambda v: isinstance(v, commands.core.Command)
+isGroup: Callable[[Any], bool] = lambda v: isinstance(v, commands.core.Group)
+isFunction: Callable[[Any], bool] = lambda v: isinstance(v, FunctionType)
+filterFunction: Callable[[Union[List, Tuple]], Union[FunctionType, None]] = lambda v: list(filter(isFunction, list(map(lambda x: x[1] or None, list(v)))))[0] or None
+filterGroup: Callable[[Union[List, Tuple]], Union[commands.core.Group, None]] = lambda v: list(filter(isGroup, list(map(lambda x: x[1] or None, list(v)))))[0] or None
 
-def isCommand(v):
-	return isinstance(v, commands.core.Command)
-
-def isCog(v):
-	return isinstance(v, commands.cog.CogMeta)
-
-def getCommandClassName(v):
-	return v.__name__
-
-def hasClassWithName(l: List, n: str):
-	res = []
-	for v in l:
-		if isinstance(v, ClassDef) and getCommandClassName(v).lower() == n.lower():
-			res.append(True)
-		else:
-			res.append(False)
-
-	if any(res) == True:
-		return True
-	else:
-		return False
-
-#testing git, idk how this works!!
 class CustomBot(commands.Bot):
 	async def setup_hook(self):
 		for file in glob.glob('./commands/**/*.py'):
 			if file.endswith('.py'):
 				dirname = os.path.basename(os.path.dirname(file))
 				filename = os.path.basename(file).split('.')[0]
-				time = datetime.now().strftime('%I:%M:%S %p')
-				print(Fore.BLUE + f'{time} ' + Fore.YELLOW + '[LOADING	] ' + Fore.LIGHTBLACK_EX + f'Loading command {filename}...', end='\r')
-				file = inspect.getmembers(importlib.import_module(f'commands.{dirname}.{filename}'), predicate=isCommand)
-				self.add_command(file[0][1])
-				print(Fore.BLUE + f'{time} ' + Fore.GREEN + '[SUCCESS	] ' + Fore.LIGHTBLACK_EX + f'Loaded command {filename}		')
+				if not filename.startswith('_'):
+					time = datetime.now().strftime('%I:%M:%S %p')
+					cmds = list(inspect.getmembers(importlib.import_module(f'commands.{dirname}.{filename}'), predicate=isCommand))
+					if len(cmds) > 1 or any(isGroup(cmd) for cmd in cmds):
+						main = filterGroup(cmds)
+						if main is None:
+							continue
+						self.add_command(main)
+					else:
+						self.add_command(cmds[0][1])
+				else:
+					continue
 
-		for file in os.listdir('./bot_events'):
+		for file in os.listdir('./events'):
 			if file.endswith('.py'):
 				evname = os.path.basename(file).split('.')[0]
-				time = datetime.now().strftime('%I:%M:%S %p')
-				print(Fore.BLUE + f'{time} ' + Fore.YELLOW + '[LOADING	] ' + Fore.LIGHTBLACK_EX + f'Loading event {evname}...', end='\r')
-				listn: Callable = inspect.getmembers(importlib.import_module(f'bot_events.{evname}'), predicate=isCallable)[0][1]
-				self.add_listener(listn)
-				print(Fore.BLUE + f'{time} ' + Fore.GREEN + '[SUCCESS	] ' + Fore.LIGHTBLACK_EX + f'Loaded event {evname}		')
+				if not evname.startswith('_'):
+					time = datetime.now().strftime('%I:%M:%S %p')
+					listn = filterFunction(list(inspect.getmembers(importlib.import_module(f'events.{evname}'), predicate=isCallable)))
+					if listn is None:
+						continue
+					self.add_listener(listn)
+					print(f'{time} || Loaded event {evname}')
+				else:
+					continue
 
-client = CustomBot(command_prefix=(settings.PREFIX), help_command = None, intents=intents, heartbeat_timeout=60000)
-client.run(settings.TOKEN)
-#FUCK YOU AMIN AM USING MY OWN WAY
+client = CustomBot(command_prefix=settings.PREFIX or '$$', help_command = None, intents=intents, heartbeat_timeout=60000)
+client.run(format(settings.TOKEN or os.environ.get('TOKEN')))
