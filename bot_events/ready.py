d634789707b298c040bd08a2ca7d0d2b92dc2bc2
Connek <106320267+connekitius@users.noreply.github.com>
2022-11-11 11:30:15 +0000

    🔇 Remove unneccessary logs and modules

--- bot_events/ready.py
+++ bot_events/ready.py
@@ -1,10 +1,2 @@
-from colorama import Fore
-
-Blue = Fore.BLUE
-Green = Fore.GREEN
-
 async def on_ready():
-	print(Green+ "Logged in!")
-	print(Blue+ "Username: ", Red+ f"{client.user.name}")
-    	print(Blue+ "id:", Red+ f"{client.user.id}")
-    	print("")
+	print(f'Logged in!')
